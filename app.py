cc4cd5a68e1193f1a5cccc5dc1b78aeb5edc1fa4
Higor Moreira <167116708+HigorMra@users.noreply.github.com>
2024-04-16 16:13:58 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,13 @@
+nome = "Higor" string
+idade = 20 int
+altura = 1.85 float
+is_male = True / False bool
+gosto_de =  "Programação"
+
+print(f"Meu nome é {nome}.")
+print(f"Eu gosto de {gosto_de}")
+print(f"Eu tenho {idade} anos")
+
+idade = "20"
+
+print(f"{nome} gosta de {gosto_de} e tem {idade} anos.")
